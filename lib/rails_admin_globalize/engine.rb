cee2b55eda7f11d8d14b9d73d2e164e54faeb041
Ben Hutton <benhutton@gmail.com>
2014-07-29 16:50:29 -0400

    Handle assets better

--- lib/rails_admin_globalize/engine.rb
+++ lib/rails_admin_globalize/engine.rb
@@ -1,11 +1,7 @@
 module RailsAdminGlobalize
   class Engine < ::Rails::Engine
-
-    if Rails.version >= '3.1'
-      initializer :assets do |config|
-        Rails.application.config.assets.precompile += %w( rails_admin/custom/rails_admin_globalize.js rails_admin/custom/rails_admin_globalize.css )
-      end
+    initializer "RailsAdminGlobalize precompile hook", group: :all do |app|
+      app.config.assets.precompile += %w( rails_admin/custom/rails_admin_globalize.js rails_admin/custom/rails_admin_globalize.css )
     end
-
   end
 end
